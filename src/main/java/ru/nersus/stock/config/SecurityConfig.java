3234d4a62c9101d4f35599feca8d34ea39d15977
Maksim <nersus1337@mail.ru>
2026-06-05 15:48:04 +0700

    Refactoring

--- src/main/java/ru/nersus/stock/config/SecurityConfig.java
+++ src/main/java/ru/nersus/stock/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package ru.nersus.stock.config;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -33,7 +31,7 @@ public class SecurityConfig {
                 })
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/")
+                        .defaultSuccessUrl("/", true)
                 )
                 .userDetailsService(myUserDetailsService)
                 .httpBasic(withDefaults());
