9bb1c139e3bbf71ccae2065979ec165a5ce890c1
maksim <nersus1337@mail.ru>
2026-07-17 21:31:13 +0700

    Add GitHub actions workflow

--- src/test/java/ru/nersus/stock/controller/MainControllerTest.java
+++ src/test/java/ru/nersus/stock/controller/MainControllerTest.java
@@ -1,6 +1,5 @@
 package ru.nersus.stock.controller;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled
 class MainControllerTest {
 
     @Autowired
