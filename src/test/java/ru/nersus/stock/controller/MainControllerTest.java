fe081f5c84ace63a59fb322bcb96ad189bd8dd16
maksim <nersus1337@mail.ru>
2026-08-11 23:18:39 +0700

    Fix some tests

--- src/test/java/ru/nersus/stock/controller/MainControllerTest.java
+++ src/test/java/ru/nersus/stock/controller/MainControllerTest.java
@@ -1,24 +1,47 @@
 package ru.nersus.stock.controller;
 
-import org.junit.jupiter.api.Disabled;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.nersus.stock.jwt.JwtProvider;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import javax.crypto.SecretKey;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled
 class MainControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockitoBean
+    JwtProvider jwtProvider;
+
+    private final SecretKey jwtAccessSecret;
+
+    public MainControllerTest(
+            @Value("${jwt.secret.access}") String jwtAccessSecret
+    ) {
+        this.jwtAccessSecret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtAccessSecret));
+    }
 
     @Test
     void getLandingRedirect() throws Exception {
@@ -28,16 +51,39 @@ class MainControllerTest {
 
     @Test
     void getLandingSuccess() throws Exception {
-        mockMvc.perform(get("/?symbol=AAPL&period=WEEKLY"))
+        mockMvc.perform(get("/?symbol=AFLT&period=month"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("stockLabels", "stockPrices"));
+                .andExpect(model().attributeExists("landing"));
     }
 
     @Test
     void getPortfolioSuccess() throws Exception {
-        mockMvc.perform(get("/portfolio").with(httpBasic("admin2@mail.ru", "111111")))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("stocks", "portfolioCost"));
+        // Given
+        final LocalDateTime now = LocalDateTime.now();
+        final Instant accessExpirationInstant = now.plusMinutes(5).atZone(ZoneId.systemDefault()).toInstant();
+        final Date accessExpiration = Date.from(accessExpirationInstant);
+        String accessToken = Jwts.builder()
+                .subject("email")
+                .id(String.valueOf(1))
+                .expiration(accessExpiration)
+                .signWith(jwtAccessSecret)
+                .compact();
+        //String accessToken = jwtProvider.generateAccessToken("email", 1);
+        Cookie cookie = new Cookie("accessToken", accessToken);
+
+        Mockito.when(jwtProvider.validateAccessToken(cookie.getValue())).thenReturn(true);
+        Mockito.when(jwtProvider.getAccessClaims(cookie.getValue()))
+                .thenReturn(
+                        Jwts
+                                .parser()
+                                .verifyWith(jwtAccessSecret)
+                                .build()
+                                .parseSignedClaims(accessToken)
+                                .getPayload()
+                );
+        // When & Then
+        mockMvc.perform(get("/portfolio").cookie(cookie))
+                .andExpect(status().isFound());
     }
 
 }
